405dcc3e342577826e6651d8cf1d892365a74dd1
[200~https://github.com/syedjamilhussain/js.assignment1.git~ <syedjamilhussain2999@gmail.com>
2024-02-21 17:20:33 +0500

    navigations fixed

--- js/utils.js
+++ js/utils.js
@@ -2,9 +2,9 @@ function gotoCreatePostPage(auth, event) {
   const user = auth.currentUser;
 
   console.log({ user });
-
+  console.log(location.href, location);
   if (user) {
-    location.href = `${location.origin}/createPost.html`;
+    location.pathname = location.pathname + "createPost.html";
   } else {
     // location.href = "/signin.html";
     alert("Please login first!");
